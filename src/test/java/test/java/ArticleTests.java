56ca7efdffa75cbc634638cb0e3745b9391ceb05
Dmitry <komov93@gmail.com>
2023-07-12 11:15:15 +0300

    Initial commit

---
+++ src/test/java/test/java/ArticleTests.java
@@ -0,0 +1,51 @@
+package test.java;
+
+import libs.CoreTestCase;
+import libs.ui.ArticlePageObject;
+import libs.ui.SearchPageObject;
+import org.junit.Test;
+
+
+public class ArticleTests extends CoreTestCase {
+    @Test
+    public void testCompareArticleTitle() {
+        SearchPageObject SearchPageObject = new SearchPageObject(driver);
+
+        SearchPageObject.initSearchInput();
+        SearchPageObject.typeSearchLine("Java");
+        SearchPageObject.clickByArticleWithSubstring("Object-oriented programming language");
+
+        ArticlePageObject ArticlePageObject = new ArticlePageObject(driver);
+        String article_title = ArticlePageObject.getArticleTitle();
+
+        assertEquals(
+                "We see unexpected title",
+                "Java (programming language)",
+                article_title
+        );
+    }
+    @Test
+    public void testSwipeArticle() {
+        SearchPageObject SearchPageObject = new SearchPageObject(driver);
+
+        SearchPageObject.initSearchInput();
+        SearchPageObject.typeSearchLine("Appium");
+        SearchPageObject.clickByArticleWithSubstring("Appium");
+
+        ArticlePageObject ArticlePageObject = new ArticlePageObject(driver);
+        ArticlePageObject.waitForTitleElement();
+        ArticlePageObject.swipeToFooter();
+
+    }
+    @Test
+    public void testCheckTitleElementInArticle() {
+        SearchPageObject SearchPageObject = new SearchPageObject(driver);
+        SearchPageObject.initSearchInput();
+        String search_line = "Java";
+        SearchPageObject.typeSearchLine(search_line);
+        SearchPageObject.clickByArticleWithSubstring("Object-oriented programming language");
+
+        ArticlePageObject ArticlePageObject = new ArticlePageObject(driver);
+        ArticlePageObject.waitForTitleElement();
+    }
+}
---
+++ src/test/java/test/java/SearchTests.java
@@ -0,0 +1,72 @@
+package test.java;
+
+import libs.CoreTestCase;
+import libs.ui.SearchPageObject;
+import org.junit.Test;
+
+
+public class SearchTests extends CoreTestCase {
+
+    @Test
+    public void testSearch() {
+        SearchPageObject SearchPageObject = new SearchPageObject(driver);
+
+        SearchPageObject.initSearchInput();
+        SearchPageObject.typeSearchLine("Java");
+        SearchPageObject.waitForSearchResult("Object-oriented programming language");
+    }
+    @Test
+    public void testCancelSearch() {
+        SearchPageObject SearchPageObject = new SearchPageObject(driver);
+
+        SearchPageObject.initSearchInput();
+        SearchPageObject.waitForCancelButtonToAppear();
+        SearchPageObject.clickCancelSearch();
+        SearchPageObject.waitForCancelButtonToDisappear();
+
+    }
+    @Test
+    public void testAmountOfNotEmptySearch() {
+        SearchPageObject SearchPageObject = new SearchPageObject(driver);
+        SearchPageObject.initSearchInput();
+        String search_line = "Linkin Park Discography";
+        SearchPageObject.typeSearchLine(search_line);
+        int amount_of_search_results = SearchPageObject.getAmountOfFoundArticles();
+
+        assertTrue(
+                "We found too few results!",
+                amount_of_search_results > 0
+        );
+    }
+    @Test
+    public void testAmountOfEmptySearch() {
+        SearchPageObject SearchPageObject = new SearchPageObject(driver);
+        SearchPageObject.initSearchInput();
+        String search_line = "xzczsavadbsbs";
+        SearchPageObject.typeSearchLine(search_line);
+        SearchPageObject.waitForEmptyResultsLabel();
+        SearchPageObject.assertThereIsNoResultOfSearch();
+    }
+    @Test
+    public void testIsSearchLineInputPresentInSearchResultList() {
+        SearchPageObject SearchPageObject = new SearchPageObject(driver);
+        SearchPageObject.initSearchInput();
+        String search_line = "Java";
+        SearchPageObject.typeSearchLine(search_line);
+        SearchPageObject.isSearchLineInputInSearchResultList(search_line);
+    }
+    @Test
+    public void testCompareArticleTitleAndCancelSearch() {
+
+        SearchPageObject SearchPageObject = new SearchPageObject(driver);
+        SearchPageObject.initSearchInput();
+        String search_line = "Java";
+        SearchPageObject.typeSearchLine(search_line);
+        SearchPageObject.assertSearchResultElementHasText(search_line);
+        String search_line2 = "JavaScript";
+        SearchPageObject.assertSearchResultElementHasText(search_line2);
+        SearchPageObject.clickCancelSearch();
+        SearchPageObject.clickCancelSearch();
+        SearchPageObject.waitForCancelButtonToDisappear();
+    }
+}
