1db7444e53ecb7541c9207788b0cf63470ae489f
Dmitry <komov93@gmail.com>
2023-07-19 18:51:11 +0300

    TestSuite

---
+++ src/test/java/suites/TestSuite.java
@@ -0,0 +1,13 @@
+package suites;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import tests.SearchTests;
+
+@RunWith(Suite.class)
+
+@Suite.SuiteClasses(
+        SearchTests.class
+)
+public class TestSuite {
+}
